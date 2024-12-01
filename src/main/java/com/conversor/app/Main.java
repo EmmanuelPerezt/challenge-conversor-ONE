9e0431ba196e4e6040b76dd31d48737d47ce208f
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2024-11-30 23:28:52 -0600

    add:funcional menu

--- src/main/java/com/conversor/app/Main.java
+++ src/main/java/com/conversor/app/Main.java
@@ -1,9 +1,9 @@
 package com.conversor.app;
 
-
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Menu.menu();
 
--- src/main/java/com/conversor/app/Menu.java
+++ src/main/java/com/conversor/app/Menu.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Menu {
 
-    public static void menu() {
+    public static void menu() throws IOException, InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
         int option = 0;
@@ -25,7 +25,21 @@ public class Menu {
             System.out.println("\nEliga una opcion:");
             option = scanner.nextInt();
             if (option==1) {
-                System.out.println("opcion uno ejcutandose");
+                scanner.nextLine();
+                System.out.println("ingrese la divisa que quiere convertir ejemplo: EUR, MXN, USD");
+                String pair1=scanner.nextLine().toUpperCase();
+                System.out.println("ingrese la divisa que quiere como resultado ejemplo EUR, MXN, USD");
+                String pair2=scanner.nextLine().toUpperCase();
+                System.out.println("ingrese la cantidad que quiere como resultado en numero entero:");
+                int amount = scanner.nextInt();
+                float result = opcion1(pair1,pair2,amount);
+
+
+                System.out.println(String.format("\n\n\nel resultado es: %f %s", result, pair2));
+                scanner.nextLine();
+
+
+            
             }else if (option==2) {
                 break;
             }   
@@ -33,13 +47,11 @@ public class Menu {
         scanner.close();
     }
 
-    void opcion1(String value1, String value2, int amount){
-        try {
+    static float opcion1(String value1, String value2, int amount) throws IOException, InterruptedException{
             float response = Request.request(value1, value2, amount);
             System.out.println("el valor es:" + response);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+            return response;
+      
 
     }
 }
